3f64b70bb661e56b2a0b1bdb3d4999ede1444aa3
williamimmanuelabadi <74845762+williamimmanuelabadi@users.noreply.github.com>
2023-12-19 18:44:43 +0800

    vgg to cnn

---
+++ age/CNN/train.py
@@ -0,0 +1,147 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+DATA_DIR = "/content/drive/MyDrive/2023DeepLearning/UTKFace"
+#DATA_DIR = "/content/data"
+TRAIN_TEST_SPLIT = 0.7
+IM_WIDTH = IM_HEIGHT = 198
+ID_GENDER_MAP = {0: 'male', 1: 'female'}
+GENDER_ID_MAP = dict((g, i) for i, g in ID_GENDER_MAP.items())
+ID_RACE_MAP = {0: 'white', 1: 'black', 2: 'asian', 3: 'indian', 4: 'others'}
+RACE_ID_MAP = dict((r, i) for i, r in ID_RACE_MAP.items())
+
+ID_GENDER_MAP, GENDER_ID_MAP, ID_RACE_MAP, RACE_ID_MAP
+
+def parse_filepath(filepath):
+    try:
+        path, filename = os.path.split(filepath)
+        filename, ext = os.path.splitext(filename)
+        age, gender, race, _ = filename.split("_")
+        return int(age), ID_GENDER_MAP[int(gender)], ID_RACE_MAP[int(race)]
+    except Exception as e:
+        print(filepath)
+        return None, None, None
+    
+# create a pandas data frame of images, age, gender and race
+files = glob.glob(os.path.join(DATA_DIR, "*.jpg"))
+attributes = list(map(parse_filepath, files))
+
+df = pd.DataFrame(attributes)
+df['file'] = files
+print(df.size)
+df.columns = ['age', 'gender', 'race', 'file']
+df = df.dropna()
+df.head()
+df.describe()
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
+_ = sns.boxplot(data=df, x='gender', y='age', ax=ax1)
+_ = sns.boxplot(data=df, x='race', y='age', ax=ax2)
+
+plt.figure(figsize=(15, 6))
+sns.boxplot(data=df, x='gender', y='age', hue='race')
+df.groupby(by=['race', 'gender'])['age'].count().plot(kind='bar')
+df['age'].hist()
+df['age'].describe()
+df = df[(df['age'] > 10) & (df['age'] < 65)]
+
+p = np.random.permutation(len(df))
+train_up_to = int(len(df) * TRAIN_TEST_SPLIT)
+train_idx = p[:train_up_to]
+test_idx = p[train_up_to:]
+
+# split train_idx further into training and validation set
+train_up_to = int(train_up_to * 0.7)
+train_idx, valid_idx = train_idx[:train_up_to], train_idx[train_up_to:]
+
+df['gender_id'] = df['gender'].map(lambda gender: GENDER_ID_MAP[gender])
+df['race_id'] = df['race'].map(lambda race: RACE_ID_MAP[race])
+
+max_age = df['age'].max()
+len(train_idx), len(valid_idx), len(test_idx), max_age
+
+from keras.utils import to_categorical
+from PIL import Image
+
+def get_data_generator(df, indices, for_training, batch_size=16):
+    images, ages, races, genders = [], [], [], []
+    while True:
+        for i in indices:
+            r = df.iloc[i]
+            file, age, race, gender = r['file'], r['age'], r['race_id'], r['gender_id']
+            im = Image.open(file)
+            im = im.resize((IM_WIDTH, IM_HEIGHT))
+            im = np.array(im) / 255.0
+            images.append(im)
+            ages.append(age / max_age)
+            races.append(to_categorical(race, len(RACE_ID_MAP)))
+            genders.append(to_categorical(gender, 2))
+            if len(images) >= batch_size:
+                yield np.array(images), [np.array(ages), np.array(races), np.array(genders)]
+                images, ages, races, genders = [], [], [], []
+        if not for_training:
+            break
+
+from keras.layers import Input, Dense, BatchNormalization, Conv2D, MaxPool2D, GlobalMaxPool2D, Dropout
+from keras.optimizers import SGD
+from keras.models import Model
+
+def conv_block(inp, filters=32, bn=True, pool=True):
+    _ = Conv2D(filters=filters, kernel_size=3, activation='relu')(inp)
+    if bn:
+        _ = BatchNormalization()(_)
+    if pool:
+        _ = MaxPool2D()(_)
+    return _
+
+input_layer = Input(shape=(IM_HEIGHT, IM_WIDTH, 3))
+_ = conv_block(input_layer, filters=32, bn=False, pool=False)
+_ = conv_block(_, filters=32*2)
+_ = conv_block(_, filters=32*3)
+_ = conv_block(_, filters=32*4)
+_ = conv_block(_, filters=32*5)
+_ = conv_block(_, filters=32*6)
+bottleneck = GlobalMaxPool2D()(_)
+
+# for age calculation
+_ = Dense(units=128, activation='relu')(bottleneck)
+age_output = Dense(units=1, activation='sigmoid', name='age_output')(_)
+
+# for race prediction
+_ = Dense(units=128, activation='relu')(bottleneck)
+race_output = Dense(units=len(RACE_ID_MAP), activation='softmax', name='race_output')(_)
+
+# for gender prediction
+_ = Dense(units=128, activation='relu')(bottleneck)
+gender_output = Dense(units=len(GENDER_ID_MAP), activation='softmax', name='gender_output')(_)
+
+model = Model(inputs=input_layer, outputs=[age_output, race_output, gender_output])
+model.compile(optimizer='rmsprop',
+              loss={'age_output': 'mse', 'race_output': 'categorical_crossentropy', 'gender_output': 'categorical_crossentropy'},
+              loss_weights={'age_output': 2., 'race_output': 1.5, 'gender_output': 1.},
+              metrics={'age_output': 'mae', 'race_output': 'accuracy', 'gender_output': 'accuracy'})
+model.summary()
+
+from keras.callbacks import ModelCheckpoint
+
+batch_size = 64
+valid_batch_size = 64
+train_gen = get_data_generator(df, train_idx, for_training=True, batch_size=batch_size)
+valid_gen = get_data_generator(df, valid_idx, for_training=True, batch_size=valid_batch_size)
+
+callbacks = [
+    ModelCheckpoint("./model_checkpoint", monitor='val_loss')
+]
+
+history = model.fit_generator(train_gen,
+                    steps_per_epoch=len(train_idx)//batch_size,
+                    epochs=10,
+                    callbacks=callbacks,
+                    validation_data=valid_gen,
+                    validation_steps=len(valid_idx)//valid_batch_size)
+model.save("age.h5")
